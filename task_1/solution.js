3e9e9b06618c261d43fe760bd92f4ce03948b63a
Elena test <khominich@nabix.ru>
2023-02-23 21:07:13 +0300

    task done all2

--- task_1/solution.js
+++ task_1/solution.js
@@ -4,16 +4,16 @@ function addInCartMessage(title, price) {
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
-let message; 
-message = productName;
-message += ' ';
-message += 'за'; 
-message += ' ';
-message += productPrice;
-message += ' ';
-message += 'теперь в корзине!';
+    let message; 
+    message = productName;
+    message += ' ';
+    message += 'за'; 
+    message += ' ';
+    message += productPrice;
+    message += ' ';
+    message += 'теперь в корзине!';
 
-console.log(message);
+  console.log(message);
 
     // Конец решения задания №1.1.
 
@@ -25,9 +25,9 @@ function addInCartCountChange(value) {
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
 
-let newValue;
-newValue = oldValue;
-newValue += 1;
+    let newValue;
+    newValue = oldValue;
+    newValue += 1;
 
     // Конец решения задания №1.2.
 
@@ -40,9 +40,9 @@ function addInCartSumChange(sum, delta) {
 
     // Задание №1.3. Увеличение суммы в корзине
 
-let newSum = oldSum + difference;
-let newSumText = newSum + ' ₽';
-newSumText = `${newSum} ₽`;
+    let newSum = oldSum + difference;
+    let newSumText = newSum + ' ₽';
+    newSumText = `${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
